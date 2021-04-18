7cd04a3b485e88c16b699c37ac571ed5e112ac0e
Taylor Lowery <admiral.taylor@gmail.com>
2021-04-17 22:31:44 -0700

    minimal implementation matching requirements works

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-if __name__ == "main":
-    pass #lol
+from transposer import Transposer
+
+def transpose(path):
+    try:
+        (longest, transposed) = Transposer().get_longest_and_transposed_word_from_file(path)
+        print(longest)
+        print(transposed)
+    except Exception as e:
+        print(e)
+
+
+transpose("./files/abcde.txt")
--- transposer.py
+++ transposer.py
@@ -6,14 +6,15 @@ class Transposer():
     def __init__(self):
         pass
 
-    def get_files_in_directory(directory):
+    def get_files_in_directory(self, directory):
         pass
 
     # Read the contents of a file, find the longest word, reverse(transpose) the word, and return the longest and transposed word
-    def get_longest_and_transposed_word_from_file(filepath):
-        file_contents = read_file(filepath)
-        longest = get_longest_word(file_contents)
-        reversed_word = reverse_string(file_contents)
-        return (longest, reversed_word)
-
-    
+    def get_longest_and_transposed_word_from_file(self, filepath):
+        try:
+            file_contents = read_file(filepath)
+            longest = get_longest_word(file_contents)
+            reversed_word = reverse_string(longest)
+            return (longest, reversed_word)
+        except Exception as e:
+            print(e)
